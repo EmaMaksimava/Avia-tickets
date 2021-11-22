29e6a12468a95936a8f29a6aecd28878ec1f1924
Emma Maksimava <cielvibritania@gmail.com>
2021-11-22 16:16:23 +0100

    modified airline api

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -6,23 +6,35 @@ class Locations {
         this.countries = null;
         this.cities = null;
         this.shortCitiesList = null;
+        this.airlines = {};
     }
     async init() {
         const response = await Promise.all([
             this.api.countries(),
             this.api.cities(),
+            this.api.airlines(),
         ]);
 
-        const [countries, cities] = response;
+        const [countries, cities, airlines] = response;
         this.countries = this.serializeCountries(countries);
         this.cities = this.serializeCities(cities);
         this.shortCitiesList = (this.createShortCitiesList(this.cities));
+        this.airlines = this.serializeAirlines(airlines);
+        console.log(this.airlines);
         return response;
 
     }
 
     getCityCodeByKey(key) {
         return this.cities[key].code;
+    }
+
+    getAirlineNameByCode(code) {
+        return this.airlines[code] ? this.airlines[code].name : '';
+    }
+
+    getAirlineLogoByCode(code) {
+        return this.airlines[code] ? this.airlines[code].logo : '';
     }
 
     createShortCitiesList(cities) {
@@ -33,6 +45,15 @@ class Locations {
             acc[key] = null;
             return acc;
         }, {})
+    }
+
+    serializeAirlines(airlines) {
+        return airlines.reduce((acc, item) => {
+            item.logo = `http://pics.avs.io/200/200/${item.code}.png`;
+            item.name = item.name || item.name_translations.en;
+            acc[item.code] = item;
+            return acc;
+        }, {});
     }
 
     serializeCountries(countries) {
