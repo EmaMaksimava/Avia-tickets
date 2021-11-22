ab3d450d30003e6be23eb69e985534dc5ccc068d
Emma Maksimava <cielvibritania@gmail.com>
2021-11-22 23:39:11 +0100

    modified cities api

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -6,6 +6,7 @@ class Locations {
         this.countries = null;
         this.cities = null;
         this.shortCitiesList = null;
+        this.lastSearch = {};
         this.airlines = {};
     }
     async init() {
@@ -20,13 +21,17 @@ class Locations {
         this.cities = this.serializeCities(cities);
         this.shortCitiesList = (this.createShortCitiesList(this.cities));
         this.airlines = this.serializeAirlines(airlines);
-        console.log(this.airlines);
         return response;
 
     }
 
     getCityCodeByKey(key) {
-        return this.cities[key].code;
+        const city = Object.values(this.cities).find((item) => item.full_name === key);
+        return city.code;
+    }
+
+    getCityNameByCode(code) {
+        return this.cities[code].name;
     }
 
     getAirlineNameByCode(code) {
@@ -40,9 +45,9 @@ class Locations {
     createShortCitiesList(cities) {
         // { 'City, Country': null}
         // Object.entries => [key, value]
-        return Object.entries(cities).reduce((acc, [key]) => {
+        return Object.entries(cities).reduce((acc, [, city]) => {
             //console.log(key);
-            acc[key] = null;
+            acc[city.full_name] = null;
             return acc;
         }, {})
     }
@@ -69,8 +74,12 @@ class Locations {
         return cities.reduce((acc, city) => {
           const country_name = this.getCountryNameByCode(city.country_code);
           const city_name = city.name || city.name_traslations.en;
-          const key = `${city_name},${country_name}`;
-          acc[key] = city;
+          const full_name = `${city_name},${country_name}`;
+          acc[city.code] = {
+              ...city,
+              country_name,
+              full_name,
+          };
           return acc;
         }, {});
     }
@@ -81,7 +90,20 @@ class Locations {
 
     async fetchTickets(params) {
         const response = await this.api.prices(params);
-        console.log(response);
+        this.lastSearch = response.data;
+    }
+
+    serializeTickets(tickets) {
+        return Object.values(tickets).map(ticket => {
+            return {
+                ...ticket,
+                origin_name: this.getCityNameByCode(ticket.origin),
+                destination_name: this.getCityNameByCode(ticket.destination),
+                airline_logo: this.getAirlineLogoByCode(ticket.airline),
+                airline_name: this.getAirlineNameByCode(ticket.airline),
+
+            };
+        });
     }
 }
 
