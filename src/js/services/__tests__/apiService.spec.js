619e8ef8ccae73f5d79be5b8a12200709c84801e
Emma Maksimava <cielvibritania@gmail.com>
2022-01-22 17:06:44 +0100

    add api-services tests

--- src/js/services/__tests__/apiService.spec.js
+++ src/js/services/__tests__/apiService.spec.js
@@ -5,6 +5,8 @@ import axios from 'axios'
 jest.mock('axios')
 
 const cities = [{ country_code: "BY", name:'Minsk', code:'MN' }]
+const countries = [{ code: 'RUS', name: 'Russion Federation'}]
+const airlines = [{ country: 'Turkey', code: 'TA', name:'Turkish Airlines'}]
 
 describe('Test API service', () => {
     it('Success fetch cities', async () => {
@@ -17,5 +19,29 @@ describe('Test API service', () => {
         const errMsg = 'Api Error'
         axios.get.mockImplementationOnce(() => Promise.reject(new Error(errMsg)))
         await expect(api.cities()).rejects.toThrow(errMsg)
+    })
+
+    it('Success fetch countries', async () => {
+        axios.get.mockImplementationOnce(() => Promise.resolve({ data: countries}))
+        await expect(api.countries()).resolves.toEqual(countries)
+        expect(axios.get).toHaveBeenCalledWith(`${config.url}/countries`)
+    })
+
+    it('Fetch countries failure', async () => {
+        const errMsg = 'Api Error'
+        axios.get.mockImplementationOnce(() => Promise.reject(new Error(errMsg)))
+        await expect(api.countries()).rejects.toThrow(errMsg)
+    })
+
+    it('Success fetch airlines', async () => {
+        axios.get.mockImplementationOnce(() => Promise.resolve({ data: airlines}))
+        await expect(api.airlines()).resolves.toEqual(airlines)
+        expect(axios.get).toHaveBeenCalledWith(`${config.url}/airlines`)
+    })
+
+    it('Fetch airlines failure', async () => {
+        const errMsg = 'Api Error'
+        axios.get.mockImplementationOnce(() => Promise.reject(new Error(errMsg)))
+        await expect(api.airlines()).rejects.toThrow(errMsg)
     })
 })
