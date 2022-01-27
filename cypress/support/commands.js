eeefd4575c32d3981de02abb2d7c4ec28643c9c1
Emma Maksimava <cielvibritania@gmail.com>
2022-01-27 19:45:51 +0100

    add final commands tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,31 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add( 'initElements', () => {
+    cy.get('[data-hook=autocompliteOrigin]').as('autocompliteOrigin')
+    cy.get('[data-hook=autocompleteDestination]').as('autocompleteDestination')
+    cy.get('[data-hook=datepickerDepartInput]').as('datepickerDepartInput')
+    cy.get('[data-hook=datepickerDepartWrap] .datepicker-modal').as('modalWindow')
+    cy.get('[data-hook=submitButton]').as('submitButton')
+    cy.get('[data-hook=resetButton]').as('resetButton')
+})
+
+Cypress.Commands.add('fillTheForm', () => {
+    cy.get('@resetButton').click()
+
+    cy.get('[data-hook=autocompliteOrigin]').type('Краков')
+    cy.get('.field-origin .autocomplete-content li:first').contains('Краков,Польша').click()
+
+    cy.get('@autocompleteDestination').type('Амстердам')
+    cy.get('.field-destination .autocomplete-content li:first').contains('Амстердам,Нидерланды').click()
+
+    cy.get('@datepickerDepartInput').click()
+    cy.get('@modalWindow').should('be.visible')
+
+    cy.get('[data-hook=datepickerDepartWrap] .datepicker-modal .is-today').as('today')
+    cy.get('[data-hook=datepickerDepartWrap] .datepicker-modal .btn-flat').as('modalBtns')
+
+    cy.get('@today').click()
+    cy.get('@today').should('have.class', 'is-selected')
+    cy.get('@modalBtns').contains('Ok').click()
+})
